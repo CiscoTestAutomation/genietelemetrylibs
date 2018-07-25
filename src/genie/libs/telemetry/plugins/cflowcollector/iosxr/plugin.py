dbc37145b737f7920ad4a803717afbff7027e8c9
Zhi Li <zhil3@cisco.com>
2018-07-25 10:21:11 -0700

    add cflow as genie telemetry plugins

---
+++ src/genie/libs/telemetry/plugins/cflowcollector/iosxr/plugin.py
@@ -0,0 +1,11 @@
+''' 
+GenieTelemetry CflowCollection Plugin for IOSXR
+'''
+
+from ..plugin import Plugin as BasePlugin
+from genie.telemetry.status import WARNING
+
+
+class Plugin(BasePlugin):
+    def execution(self, device, **kwargs):    
+	    return WARNING('IOSXR not supported CflowCollection')
---
+++ src/genie/libs/telemetry/plugins/cflowcollector/nxos/plugin.py
@@ -0,0 +1,10 @@
+''' 
+GenieTelemetry CflowCollection Plugin for NXOS
+'''
+
+from ..plugin import Plugin as BasePlugin
+from genie.telemetry.status import WARNING
+
+
+class Plugin(BasePlugin):
+    pass
